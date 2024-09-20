8f107ea94a497386dfed9a7795ccfed26af43651
Ravikumar Jha <jharavi153@gmail.com>
2024-09-20 18:03:15 +0530

    array question2

---
+++ 2,Array.js
@@ -0,0 +1,6 @@
+let fruits = ['apple', 'mango', 'banana'];
+fruits.push("cherry");
+console.log(fruits);
+
+fruits.unshift("Pineapple");
+console.log(fruits);
--- Array.js
+++
@@ -1,4 +0,0 @@
-let fruits = ['apple', 'mango', 'banana'];
-console.log(fruits[1]);
-console.log(fruits[0]);
-console.log(fruits[2]);
