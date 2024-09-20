643da6ca0ea3be5d73d16258504b65685e590e90
Ravikumar Jha <jharavi153@gmail.com>
2024-09-20 18:09:29 +0530

    array question

--- 2,Array.js
+++ 2,Array.js
@@ -1,6 +1,14 @@
 let fruits = ['apple', 'mango', 'banana'];
-fruits.push("cherry");
+fruits.push("cherry");     //Adds Cherry to the end
 console.log(fruits);
 
-fruits.unshift("Pineapple");
+fruits.unshift("Pineapple");   // Adds Pineapple to the beginning
 console.log(fruits);
+
+console.log(fruits.length);
+
+for(let i=0;i<fruits.length; i++){    // iterate using Loop 
+    console.log(fruits[i]);
+}
+
+
