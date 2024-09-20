8751addfbd9b27f547b6867df9362c80cf02684c
Ravikumar Vishwanath Jha <120722758+jharavi26@users.noreply.github.com>
2024-09-20 18:29:54 +0530

    Update 2,Array.js

--- 2,Array.js
+++ 2,Array.js
@@ -11,4 +11,12 @@ for(let i=0;i<fruits.length; i++){    // iterate using Loop
     console.log(fruits[i]);
 }
 
+fruits.forEach((fruits, index) =>{
+    console.log(`fruits at index ${index} is ${fruits}`);  // forEach Method used to execute a provided function once for every element.
+});
+
+let moreFruits = ['Graphs','watermelon'];
+let allFruits = fruits.concat(moreFruits);  //using concat method 
+console.log(allFruits) 
+
 
