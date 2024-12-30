5e4e149ce21d3b5c5ef5e4a58b0b0f611028ca8a
Ravikumar Jha <jharavi153@gmail.com>
2024-12-30 16:39:10 +0530

    First Occurence of String

--- 03.String.js
+++ 03.String.js
@@ -20,3 +20,51 @@ function reverseString(str) {
 }
 
 console.log(reverseString("hello"));
+
+//Palindrome in String 
+
+function Palindrome(str){
+  str = str.replace(/[^a-z0-9]/gi, '').toLowerCase();
+  
+  let left =0;
+  let right = str.length-1;
+  while(left<right){
+  if(str[left] !== str[right]){
+    return false;
+  }
+  left++;
+  right--
+}
+return true;
+}
+console.log(Palindrome("madam"));
+
+//using Recursion 
+function Palindome(str, left=0, right=str.length-1){
+  if(left>=right){
+    return true;
+  }
+if(str[left].toLowerCase !== str[right].toLowerCase){
+  return false;
+}
+return Palindome(str, left+1, right-1)
+}
+console.log(Palindome("racecar"));
+
+//Frequency of the First non-repeating Character 
+
+function First(str){
+  let Frequency = {};
+  for(let i=0; i<str.length; i++)
+  {
+    let char = str[i];
+    Frequency[char] = (Frequency[char] ||0)+1;
+  }
+  for(let i=0; i<str.length; i++){
+    if (Frequency[str[i]]===1){
+      return str[i];
+    }
+  }
+return null;
+}
+console.log(First("raviraj"));
