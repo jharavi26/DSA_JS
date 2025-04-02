50d5de0cd3e94a7ab6aff327603176d7145e392b
Ravikumar Jha <jharavi153@gmail.com>
2025-04-02 23:08:00 +0530

    Improvement

--- 10.Recursion.js
+++ 10.Recursion.js
@@ -161,6 +161,15 @@ console.log(sum([1,2,3,4]))
 
 //Largest Element in Array 
 
+function sum(arr){
+
+  if(arr.length === 1) return arr[0];
+
+    return Math.max(arr[0] , sum(arr.slice(1)))
+}
+
+console.log(sum([1,2,3,4,5]))
+
 //Linear Search in Array 
 
 
