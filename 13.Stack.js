b3cd2970c24c7f2330a9e285aa21cc5468e2e2b3
Ravikumar Jha <jharavi153@gmail.com>
2025-04-05 20:40:51 +0530

    Add Stack question

--- 13.Stack.js
+++ 13.Stack.js
@@ -35,3 +35,65 @@ stack.push(20);
 stack.push(30);
 console.log(stack.size())
 console.log(stack.peek());
+
+//Balanced Paraanthesis
+
+function balance(str){
+  stack = [];
+  const brackets = {
+    ")": "(",
+    "}": "{",
+    "]": "["
+  };
+
+  for(let char of str){
+    if(char === "(" || char === "{" || char === "["){
+      stack.push(char)
+    }
+    else if(char === ")" || char === "}" || char === "]"){
+      if(stack.length === 0   || stack.pop() !== brackets[char]){
+        return false
+      }
+    }
+  }
+  return stack.length ===0;
+}
+console.log(balance("([{}])"))
+
+
+//Postfix Operation :- Take two operand & perform operation 
+
+function evaluate(tokens){
+  let stack = [];
+
+  for(let token of tokens){
+    if(!isNaN(token)){
+      stack.push(Number(token))
+    }
+    else {
+  const a = stack.pop();
+  const b = stack.pop();
+
+  switch (token) {
+    case "+":
+      stack.push(a+b);
+      break;
+
+  case "-":
+    stack.push(a-b);
+    break;
+
+  case "*":
+  stack.push(a*b)
+  break;
+
+  case "/":
+  stack.push(Math.trunc(a+b));
+  break;
+}
+  }
+  }
+  return stack.pop();
+}
+
+console.log(evaluate(["1", "2" , "*" , "3" , "+"]))
