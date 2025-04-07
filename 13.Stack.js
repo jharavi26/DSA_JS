3d6c28e03176c50675bb2837ba26e929d016b3f6
Ravikumar Jha <jharavi153@gmail.com>
2025-04-07 20:06:32 +0530

    Add Question

--- 13.Stack.js
+++ 13.Stack.js
@@ -121,3 +121,46 @@ function gretherElement(arr){
 }
 
 console.log(gretherElement([4,5,2,10]))
+
+//Implement using Stack using two Queue
+
+class queue {
+  constructor(){
+    this.q1 = [];
+    this.q2 = [];
+  }
+push(x){
+  this.q2.push(x);
+  while(this.q1.length){
+    this.q2.push(this.q1.shift())
+  }
+  [this.q1 , this.q2] = [this.q2, this.q1]
+}
+
+pop(){
+  if(this.q1.length === 0) return null;
+  return this.q1.shift();
+  }
+
+  top(){
+    if(this.q1.length === 0) return null;
+    return this.q1[0];
+    }
+  
+
+isEmpty(){
+  return this.q1.length === 0;
+}
+}
+
+const stack1 = new queue();
+
+stack1.push(10);
+stack1.push(20);
+stack1.push(30);
+
+console.log(stack1.top());    // 30
+console.log(stack1.pop());    // 30
+console.log(stack1.top());    // 20
+console.log(stack1.isEmpty()); // false
+
